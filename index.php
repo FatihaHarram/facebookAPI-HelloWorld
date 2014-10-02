9da731afc063d061041c5ad7290a36427a4ab523
Nishad <nishad.dawkhar94@gmail.com>
2014-10-02 19:19:40 +0530

    updated

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ try {
 // see if we have a session
 if ( isset( $session ) ) {
   // graph api request for user data
-  $request = new FacebookRequest( $session, 'GET', '/me/music' );
+  $request = new FacebookRequest( $session, 'GET', '/me' );
   $response = $request->execute();
   // get response
   $graphObject = $response->getGraphObject();
